b958187beef7783c5e54448360d187166e3d7984
Rod <rod@badlydrawngames.com>
2021-05-08 13:48:22 +0100

    Fix warning.

--- conversions.h
+++ conversions.h
@@ -8,8 +8,7 @@ static inline float U32AsFloat(const uint32_t a)
     {
         uint32_t a;
         float b;
-    } x;
-    x.a = a;
+    } x = {a};
     return x.b;
 }
 
@@ -19,7 +18,6 @@ static inline uint32_t FloatAsU32(const float a)
     {
         float a;
         uint32_t b;
-    } x;
-    x.a = a;
+    } x = {a};
     return x.b;
 }
