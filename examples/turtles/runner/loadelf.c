3c9526ae897a947a986c086c0135d70b7a035bea
Rod <rod@badlydrawngames.com>
2021-08-08 16:49:53 +0100

    Match loadable segments to memory.

--- examples/turtles/runner/loadelf.c
+++ examples/turtles/runner/loadelf.c
@@ -1,6 +1,14 @@
 #include "loadelf.h"
 
+#include <stdbool.h>
 #include <stdio.h>
+#include <string.h>
+
+// ELF format references:
+// https://en.wikipedia.org/wiki/Executable_and_Linkable_Format
+// https://man7.org/linux/man-pages/man5/elf.5.html
+
+#define EI_NIDENT 16
 
 #define EI_MAG0 0
 #define EI_MAG1 1
@@ -15,9 +23,68 @@
 
 #define PT_LOAD 1
 
-void LoadElf(const char* filename)
+typedef uint32_t Elf32_Addr;
+typedef uint32_t Elf32_Off;
+
+typedef struct Elf32_Ehdr
 {
-    FILE* fp = fopen(filename, "rb");
+    unsigned char e_ident[EI_NIDENT];
+    uint16_t e_type;
+    uint16_t e_machine;
+    uint32_t e_version;
+    Elf32_Addr e_entry;
+    Elf32_Off e_phoff;
+    Elf32_Off e_shoff;
+    uint32_t e_flags;
+    uint16_t e_ehsize;
+    uint16_t e_phentsize;
+    uint16_t e_phnum;
+    uint16_t e_shentsize;
+    uint16_t e_shnum;
+    uint16_t e_shstrndx;
+} Elf32_Ehdr;
+
+typedef struct Elf32_Phdr
+{
+    uint32_t p_type;
+    Elf32_Off p_offset;
+    Elf32_Addr p_vaddr;
+    Elf32_Addr p_paddr;
+    uint32_t p_filesz;
+    uint32_t p_memsz;
+    uint32_t p_flags;
+    uint32_t p_align;
+} Elf32_Phdr;
+
+typedef struct Elf32_Shdr
+{
+    uint32_t sh_name;
+    uint32_t sh_type;
+    uint32_t sh_flags;
+    Elf32_Addr sh_addr;
+    Elf32_Off sh_offset;
+    uint32_t sh_size;
+    uint32_t sh_link;
+    uint32_t sh_info;
+    uint32_t sh_addralign;
+    uint32_t sh_entsize;
+} Elf32_Shdr;
+
+void LoadElf(const char* filename, MemoryDescriptor* memoryDescriptors, int numDescriptors)
+{
+    FILE* fp = NULL;
+
+    if (memoryDescriptors == NULL)
+    {
+        goto finish;
+    }
+
+    if (numDescriptors < 0)
+    {
+        goto finish;
+    }
+
+    fp = fopen(filename, "rb");
     if (!fp)
     {
         goto finish;
@@ -28,7 +95,12 @@ void LoadElf(const char* filename)
     {
         goto finish;
     }
-    size_t fileSize = ftell(fp);
+    long here = ftell(fp);
+    if (here < 0)
+    {
+        goto finish;
+    }
+    size_t fileSize = here;
 
     //  Bail if the file is too small.
     if (fileSize < sizeof(Elf32_Ehdr))
@@ -41,8 +113,6 @@ void LoadElf(const char* filename)
     {
         goto finish;
     }
-
-    // --- Check the ELF header ----------------------------------------------------------------------------------------------------
 
     // Read the ELF header.
     Elf32_Ehdr header;
@@ -121,20 +191,18 @@ void LoadElf(const char* filename)
         goto finish;
     }
 
-    // If we reach here then we know that we have a 32-bit RISC-V executable that we have a good chance of being able to load.
+    // If we reach here then we have a 32-bit RISC-V executable that we have a good chance of being able to load.
 
-    printf("     Program Entry point: 0x%08x\n", header.e_entry);
-
-    // --- Check the program headers -----------------------------------------------------------------------------------------------
-
-    // Go to the start of the program header table.
-    if (fseek(fp, header.e_phoff, SEEK_SET) != 0)
-    {
-        goto finish;
-    }
-
+    // Iterate through the program headers, looking for loadable segments.
+    bool entryPointValid = false;
     for (uint16_t i = 0; i < header.e_phnum; i++)
     {
+        // Go to this program header's entry in the program header table.
+        if (fseek(fp, header.e_phoff + i * header.e_phentsize, SEEK_SET) != 0)
+        {
+            goto finish;
+        }
+
         Elf32_Phdr phdr;
         if (fread(&phdr, sizeof(phdr), 1, fp) != 1)
         {
@@ -159,19 +227,59 @@ void LoadElf(const char* filename)
             goto finish;
         }
 
-        // TODO: Check that this segment will fit in memory.
+        if (phdr.p_memsz != 0)
+        {
+            bool targetSegmentFound = false;
+            for (MemoryDescriptor* m = memoryDescriptors; m < memoryDescriptors + numDescriptors; m++)
+            {
+                if (phdr.p_vaddr < m->start || phdr.p_vaddr + phdr.p_memsz >= m->start + m->size)
+                {
+                    continue;
+                }
+                targetSegmentFound = true;
+                entryPointValid = entryPointValid || (header.e_entry >= m->start && header.e_entry < m->start + m->size);
 
-        printf("\nLoadable segment\n");
-        printf("\t     File offset: 0x%08x\n", phdr.p_offset);
-        printf("\t      Image size: %d bytes\n", phdr.p_filesz);
-        printf("\t     Memory size: %d bytes\n", phdr.p_memsz);
-        printf("\tPhysical address: 0x%08x\n", phdr.p_paddr);
-        printf("\t Virtual address: 0x%08x\n", phdr.p_vaddr);
+                // Check that the memory actually points somewhere.
+                if (!m->data)
+                {
+                    goto finish;
+                }
+
+                // Go to the segment's file image.
+                if (fseek(fp, phdr.p_offset, SEEK_SET) != 0)
+                {
+                    goto finish;
+                }
+
+                // Zero the target memory.
+                void* target = (char*)m->data + (phdr.p_vaddr - m->start);
+                memset(target, 0, phdr.p_memsz);
+
+                // Load the image.
+                if (fread(target, 1, phdr.p_filesz, fp) != phdr.p_filesz)
+                {
+                    goto finish;
+                }
+
+                break;
+            }
+
+            // Bail if no memory could be found suitable to load this segment.
+            if (!targetSegmentFound)
+            {
+                goto finish;
+            }
+        }
+    }
+
+    // Bail if the program's entry point isn't valid.
+    if (!entryPointValid)
+    {
+        goto finish;
     }
 
     // If we get here then we have a valid 32-bit RISC-V executable that we can deal with.
-
-    puts("\nWe have a valid RISC-V executable.");
+    puts("\nSuccessfully loaded RISC-V executable.");
 
 finish:
     if (fp)
@@ -182,8 +290,11 @@ finish:
 
 int main(void)
 {
+    char rom[0x4000];
+    char ram[0x4000];
+    MemoryDescriptor memory[] = {{.start = 0x0, .size = 0x4000, .data = rom}, {.start = 0x4000, .size = 0x4000, .data = ram}};
     const char* filename = "examples/turtles/arviss/build/turtle";
-    LoadElf(filename);
+    LoadElf(filename, memory, sizeof(memory) / sizeof(memory[0]));
 
     return 0;
 }
--- examples/turtles/runner/loadelf.h
+++ examples/turtles/runner/loadelf.h
@@ -1,56 +1,12 @@
 #pragma once
 
-// References:
-// https://en.wikipedia.org/wiki/Executable_and_Linkable_Format
-// https://man7.org/linux/man-pages/man5/elf.5.html
-
 #include <stdint.h>
 
-#define EI_NIDENT 16
-
-typedef uint32_t Elf32_Addr;
-typedef uint32_t Elf32_Off;
-
-typedef struct Elf32_Ehdr
+typedef struct MemoryDescriptor
 {
-    unsigned char e_ident[EI_NIDENT];
-    uint16_t e_type;
-    uint16_t e_machine;
-    uint32_t e_version;
-    Elf32_Addr e_entry;
-    Elf32_Off e_phoff;
-    Elf32_Off e_shoff;
-    uint32_t e_flags;
-    uint16_t e_ehsize;
-    uint16_t e_phentsize;
-    uint16_t e_phnum;
-    uint16_t e_shentsize;
-    uint16_t e_shnum;
-    uint16_t e_shstrndx;
-} Elf32_Ehdr;
+    uint32_t start; // The start address of this memory segment in VM memory.
+    uint32_t size;  // The size of this memory segment.
+    void* data;     // This memory segment's data in host memory.
+} MemoryDescriptor;
 
-typedef struct Elf32_Phdr
-{
-    uint32_t p_type;
-    Elf32_Off p_offset;
-    Elf32_Addr p_vaddr;
-    Elf32_Addr p_paddr;
-    uint32_t p_filesz;
-    uint32_t p_memsz;
-    uint32_t p_flags;
-    uint32_t p_align;
-} Elf32_Phdr;
-
-typedef struct Elf32_Shdr
-{
-    uint32_t sh_name;
-    uint32_t sh_type;
-    uint32_t sh_flags;
-    Elf32_Addr sh_addr;
-    Elf32_Off sh_offset;
-    uint32_t sh_size;
-    uint32_t sh_link;
-    uint32_t sh_info;
-    uint32_t sh_addralign;
-    uint32_t sh_entsize;
-} Elf32_Shdr;
+void LoadElf(const char* filename, MemoryDescriptor* memoryDescriptors, int numDescriptors);
