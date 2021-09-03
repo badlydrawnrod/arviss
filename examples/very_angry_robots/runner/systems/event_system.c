cadead37e12ccbd57de3cf5b47ae0223cb8bb3a9
Rod <rod@badlydrawngames.com>
2021-09-03 11:26:26 +0100

    Identify who collided with whom.

--- examples/very_angry_robots/runner/systems/event_system.c
+++ examples/very_angry_robots/runner/systems/event_system.c
@@ -1,16 +1,39 @@
 #include "event_system.h"
 
 #include "components/events.h"
+#include "entities.h"
 #include "raylib.h"
+
+static const char* Identify(EntityId id)
+{
+    if (Entities.Is(id, bmPlayer))
+    {
+        return "player";
+    }
+    else if (Entities.Is(id, bmRobot))
+    {
+        return "robot";
+    }
+    else if (Entities.Is(id, bmWall))
+    {
+        return "wall";
+    }
+    else if (Entities.Is(id, bmDoor))
+    {
+        return "door";
+    }
+    return "*** unknown ***";
+}
 
 void UpdateEventSystem(void)
 {
     for (int i = 0, numEvents = Events.Count(); i < numEvents; i++)
     {
-        Event* e = Events.Get((EventId){.id = i});
+        const Event* e = Events.Get((EventId){.id = i});
         if (e->type == etCOLLISION)
         {
-            TraceLog(LOG_INFO, "Collision between %d and %d", e->collision.firstId.id, e->collision.secondId.id);
+            const CollisionEvent* c = &e->collision;
+            TraceLog(LOG_INFO, "Collision between %s and %s", Identify(c->firstId), Identify(c->secondId));
         }
     }
     Events.Clear();
