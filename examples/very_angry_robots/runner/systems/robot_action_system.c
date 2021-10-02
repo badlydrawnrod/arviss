ed961bc4f4340c648d28081d630ce2dd6d6d21f6
Rod Hyde <rod.hyde@gmail.com>
2021-10-02 15:13:30 +0100

    Robots move towards the player.

--- examples/very_angry_robots/runner/systems/robot_action_system.c
+++ examples/very_angry_robots/runner/systems/robot_action_system.c
@@ -5,6 +5,9 @@
 #include "systems/event_system.h"
 #include "tables/events.h"
 #include "tables/positions.h"
+#include "tables/velocities.h"
+
+#define ROBOT_SPEED 0.5f
 
 static bool isEnabled = true;
 static EntityId playerId = {.id = -1};
@@ -37,6 +40,7 @@ static bool GetMyPosition(EntityId id, float* x, float* y)
     const Vector2 myPos = Positions.GetPosition(id);
     *x = myPos.x;
     *y = myPos.y;
+
     return true;
 }
 
@@ -45,6 +49,7 @@ static bool GetPlayerPosition(EntityId id, float* x, float* y)
     const Vector2 playerPos = Positions.GetPosition(playerId);
     *x = playerPos.x;
     *y = playerPos.y;
+
     return true;
 }
 
@@ -56,6 +61,24 @@ static bool FireAt(EntityId id, float x, float y)
     const Vector2 aim = {cosf(angle), sinf(angle)};
     const Room* room = Rooms.Get(id);
     MakeRobotShot(room->roomId, p->position, aim, id);
+
+    return true;
+}
+
+static bool MoveTowards(EntityId id, float x, float y)
+{
+    const Position* p = Positions.Get(id);
+    const Vector2 robotPos = p->position;
+    const float angle = atan2f(y - robotPos.y, x - robotPos.x);
+    const Vector2 movement = {cosf(angle), sinf(angle)};
+
+    // TODO: finally, a use for the separation. Here the robot is effectively telling us where it wants to go. That'll be applied
+    //  in a different step, because if the robot doesn't move smoothly then it won't move on every tick. So, I need to figure that
+    //  out too ... is Velocity an actual velocity or it just a desired velocity?
+    //  I suspect that the movement system will have a role in this.
+    Velocity* v = Velocities.Get(id);
+    v->velocity = Vector2Scale(movement, ROBOT_SPEED);
+
     return true;
 }
 
@@ -67,15 +90,23 @@ static bool FireAt(EntityId id, float x, float y)
 static void UpdateRobot(EntityId id)
 {
     // If this was running in the VM then it'd probably be in a while loop.
+
+    // Where am I?
     float myX;
     float myY;
     GetMyPosition(id, &myX, &myY);
 
+    // Where's the player?
+    float playerX;
+    float playerY;
+    GetPlayerPosition(id, &playerX, &playerY);
+
+    // Move in the direction of the player.
+    MoveTowards(id, playerX, playerY);
+
+    // Fire at the player.
     if (((int)myX % 47) == 0)
     {
-        float playerX;
-        float playerY;
-        GetPlayerPosition(id, &playerX, &playerY);
         FireAt(id, playerX, playerY);
     }
 }
